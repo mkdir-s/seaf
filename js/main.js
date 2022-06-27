824fe7f6fdb96c68e7789e7c2e88620b30d32c4f
Alexeyenko Artyom <i.html06@gmail.com>
2022-06-27 12:15:58 +0600

    feat: add header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(function(){
-
+    $('.header__slider').slick({
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-prev.svg" alt="arrow-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-next.svg" alt="arrow-next"></button>',
+        vertical: true,
+    });
 });
